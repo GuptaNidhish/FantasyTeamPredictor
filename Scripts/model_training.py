54ee0e5501f21e75c83029cce32943a0ff512fe2
Nidhish Gupta <guptanidhishwork@gmail.com>
2026-03-13 23:27:03 +0530

    Final Model trained Finally

--- Scripts/model_training.py
+++ Scripts/model_training.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd 
 from lightgbm import LGBMRegressor
 import joblib
-data = pd.read_csv("/Users/nidhishgupta/Desktop/Dream11_Fantasy Team_Predictor/data/processed/ready_for_training.csv")
+data = pd.read_csv("/Users/nidhishgupta/Desktop/Dream11_Fantasy Team_Predictor/data/processed/full_req_data.csv")
 X_train = data.drop(columns=['fantasy_points','player','venue','season','player_role_batsman','player_role_bowler','match_id'])
 y_train = data['fantasy_points']
 best_params = {'subsample': 0.9, 'num_leaves': 127, 'min_child_samples': 5, 'max_depth': 10, 'learning_rate': 0.02, 'colsample_bytree': 0.6}
@@ -18,4 +18,5 @@ final_model.fit(
     X_train,
     y_train,
 )
+X_train.to_csv("/Users/nidhishgupta/Desktop/Dream11_Fantasy Team_Predictor/data/processed/training_data.csv")
 joblib.dump(final_model,"/Users/nidhishgupta/Desktop/Dream11_Fantasy Team_Predictor/models/point_predicter.pkl")
