4bb5bf52729872e5d2ead3506e62a9edd7b3c2cd
Nidhish <guptanidhish26@gmail.com>
2026-04-09 01:24:57 +0530

    Cloud database used

--- db/initialization.py
+++ db/initialization.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # 🔑 Replace with your actual credentials
-DATABASE_URL = "postgresql+psycopg2://postgres:Jaibaba001#@localhost:5432/fantasy_db"
+DATABASE_URL = "postgresql://postgres:Jaibaba001#@db.mpyitncpkyunkqccefit.supabase.co:5432/postgres?sslmode=require"
 
 # Engine (core connection)
 engine = create_engine(DATABASE_URL)
